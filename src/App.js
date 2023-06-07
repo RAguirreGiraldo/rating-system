98a0d5a4d6aaccae2b1627c6bb47d18129145c1c
RAguirreGiraldo <raguirregiraldo@gmail.com>
2023-06-07 11:52:18 -0500

    Update styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import Dialog from './components/Dialog';
+import StarRating from './components/StarRating';
 import './style/index.css';
 
 function App() {
   return (
     <main>
 	      <div className= 'container'>
-          <h3>This a Test</h3>
+          <h3>Rating System</h3>
+          <StarRating/>
+          <Dialog/>
+          <button>OPEN DIALOG</button>
 	      </div>
 	  </main>
   );
